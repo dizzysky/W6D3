c06899238491588ce25dd35c0a372b012774939a
Sanjid <dsanjid@gmail.com>
2023-08-03 14:05:09 -0400

    successfully migrated artworks

--- practice-for-ch-rails-first-routes-long-practice-main/FirstRoutes/db/migrate/20230803144434_change_user.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/FirstRoutes/db/migrate/20230803144434_change_user.rb
@@ -2,6 +2,6 @@ class ChangeUser < ActiveRecord::Migration[7.0]
   def change
     rename_column :users, :name, :username
     remove_column :users, :email
-    add_index: :users, :username, unique: true
+    add_index :users, :username, unique: true
   end
 end
